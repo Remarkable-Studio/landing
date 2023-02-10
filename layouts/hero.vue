dc773a93fd9abdeae6c454d1aac8b5c95c8207a8
Luc Verdier <luc.verdier@dougs.fr>
2023-02-10 17:58:27 +0400

    fix blur

--- layouts/hero.vue
+++ layouts/hero.vue
@@ -3,8 +3,8 @@
         <div
             class="layout flex relative flex-wrap p-6 md:p-10 md:h-screen justify-center content-center text-white font-body">
             <div class="absolute top-0 left-0 right-0 z-0 w-screen h-full md:h-screen overflow-hidden bg-gray-900">
-                <img src="../assets/img/blur_red.svg" class="blur-red" />
-                <img src="../assets/img/blur_yellow.svg" class="blur-yellow" />
+                <img src="~/assets/img/blur_red.svg" class="blur-red" />
+                <img src="~/assets/img/blur_yellow.svg" class="blur-yellow" />
                 <div class="noise absolute w-screen h-full opacity-20"></div>
             </div>
             <div class="max-w-6xl z-10">
@@ -16,18 +16,18 @@
 
 <style>
 .noise {
-    background-image: url("../assets/img/noise.png");
+    background-image: url("~/assets/img/noise.png");
     background-repeat: repeat;
 }
 
 .blur-red {
-    position: "absolute" !important;
+    position: absolute;
     top: 0;
     left: 0;
 }
 
 .blur-yellow {
-    position: "absolute" !important;
+    position: absolute;
     bottom: 0;
     left: 0;
 }
