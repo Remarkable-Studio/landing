0fbe49fdd86ca1e31b9471f95ba786fa731cd41d
Luc Verdier <luc.verdier@dougs.fr>
2023-02-10 08:31:43 +0400

    fix font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/i18n',
-  ],
-  tailwindcss: {
-    configPath: 'tailwind.config',
-  },
-  i18n: {
-    locales: ['en', 'fr'], 
-    defaultLocale: 'en',
-    strategy: 'prefix_except_default',
-    detectBrowserLanguage: {
-      useCookie: false,
-      redirectOn: 'root',  // recommended
+    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+    tailwindcss: {
+        configPath: "tailwind.config",
     },
-    vueI18n: {
-      fallbackLocale: 'en',
-      messages: {
-        en: {
-          title1: 'Capture, Play, Edit & Share',
-          title2: 'Your Moments From The Same Place',
-          privacy: 'Terms & Privacy'
+    css: ["@/assets/fonts.css"],
+    i18n: {
+        locales: ["en", "fr"],
+        defaultLocale: "en",
+        strategy: "prefix_except_default",
+        detectBrowserLanguage: {
+            useCookie: false,
         },
-        fr: {
-          title1: 'Capture, Joue, Edite & Partage',
-          title2: 'Tes Moments Depuis Le Même Endroit',
-          privacy: 'Conditions et confidentialité'
-        }
-      }
-    }
-  }
-})
+        vueI18n: {
+            legacy: false,
+            messages: {
+                en: {
+                    title1: "Capture, Play, Edit & Share",
+                    title2: "Your Moments From The Same Place",
+                    privacy: "Terms & Privacy",
+                },
+                fr: {
+                    title1: "Capture, Joue, Edite & Partage",
+                    title2: "Tes Moments Depuis Le Même Endroit",
+                    privacy: "Conditions et confidentialité",
+                },
+            },
+        },
+    },
+});
