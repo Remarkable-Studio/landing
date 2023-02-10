83c3e34b6005dc2b0ad7f8aa2e2954df478bf986
Luc Verdier <luc.verdier@dougs.fr>
2023-02-10 18:06:45 +0400

    update email

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -7,7 +7,7 @@ const localePath = useLocalePath();
   <div class="flex text-sm">
     <NuxtLink class="mr-4 transition opacity-50 hover:opacity-100" :to="localePath('privacy')">{{ t('privacy') }}
     </NuxtLink>
-    <a href="mailto:mickael@remarkable.school" class="transition opacity-50 hover:opacity-100">Contact</a>
+    <a href="mailto:hello@shortcam.app" class="transition opacity-50 hover:opacity-100">Contact</a>
   </div>
 </template>
 
